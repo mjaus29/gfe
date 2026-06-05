93561d6c0c1c41cc9920164a668fdc436d066251
Mark Joshua Austria <mark.a@wphdigital.com>
2026-06-05 16:22:26 +0800

    Refactor code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { metadata } from "./layout";
 
 export default function Home() {
   return (
@@ -7,7 +6,7 @@ export default function Home() {
       <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-center gap-8 bg-white dark:bg-black">
         <Image
           className="dark:invert"
-          src="/img/favicon.svg"
+          src="/img/logo.svg"
           alt="GFE logo"
           width={100}
           height={20}
@@ -15,10 +14,11 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center">
           <h1 className="max-w-md text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
-            {metadata.title}
+            GFE Challenges - Mark Austria
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            {metadata.description}
+            A collection of frontend challenges created by Mark Austria,
+            showcasing my skills and creativity in web development.
           </p>
         </div>
       </main>
