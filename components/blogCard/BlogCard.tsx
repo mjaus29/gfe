1944a86457f283c47d92780020df910fb1fb7dba
Mark Joshua Austria <mark.a@wphdigital.com>
2026-06-11 14:51:29 +0800

    fix: adjust layout and styling in BlogCard and BlogCardPage components

--- components/blogCard/BlogCard.tsx
+++ components/blogCard/BlogCard.tsx
@@ -10,18 +10,14 @@ type BlogCardProps = {
 
 const BlogCard = ({ image, title, badge, excerpt }: BlogCardProps) => {
   return (
-    <article className="mt-30 flex h-full max-w-85 flex-col rounded-lg bg-white shadow-sm">
-      <Image
-        src={image}
-        alt={title}
-        width={340}
-        height={288}
-        className="rounded-t-lg object-cover"
-      />
+    <article className="h-full w-85 overflow-hidden rounded-lg border border-gray-200 bg-white shadow-sm">
+      <div className="relative h-72 w-full">
+        <Image src={image} alt={title} fill className="object-cover" />
+      </div>
 
       <div className="flex flex-col gap-3 px-4 py-6">
         <div className="flex flex-col gap-2">
-          <span className="self-start rounded-full border border-solid border-green-200 bg-green-50 px-2 py-0.5 text-center text-sm font-normal text-green-700">
+          <span className="self-start rounded-full border border-green-200 bg-green-50 px-2 py-0.5 text-center text-sm font-normal text-green-700">
             {badge}
           </span>
           <h3 className="text-lg font-semibold text-neutral-900">{title}</h3>
@@ -30,7 +26,10 @@ const BlogCard = ({ image, title, badge, excerpt }: BlogCardProps) => {
         <div className="flex flex-col gap-6">
           <p className="text-base font-medium text-neutral-600">{excerpt}</p>
 
-          <Link href="#" className="flex items-center gap-1.5 rounded">
+          <Link
+            href="#"
+            className="flex items-center gap-1.5 rounded focus-visible:outline focus-visible:outline-indigo-700"
+          >
             <span className="text-base font-medium text-indigo-700">
               Read More
             </span>
