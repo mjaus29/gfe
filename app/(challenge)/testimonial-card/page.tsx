93ecb58cf5cdf0859047f2f863405ac534633885
Mark Joshua Austria <mark.a@wphdigital.com>
2026-06-11 13:13:00 +0800

    fix: wrap TestimonialCard in a container for improved layout and styling

--- app/(challenge)/testimonial-card/page.tsx
+++ app/(challenge)/testimonial-card/page.tsx
@@ -2,12 +2,14 @@ import TestimonialCard from "@/components/testimonialCard/designs/TestimonialCar
 
 const TestimonialCardPage = () => {
   return (
-    <TestimonialCard
-      image={"/img/profile-thumbnail.png"}
-      name={"Sarah Dole"}
-      username={"@sarahdole"}
-      message={`I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!`}
-    />
+    <div className="container-layout flex min-h-screen min-w-screen justify-center bg-linear-to-b from-gray-50 to-[#d2d6db]">
+      <TestimonialCard
+        image={"/img/profile-thumbnail.png"}
+        name={"Sarah Dole"}
+        username={"@sarahdole"}
+        message={`I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!`}
+      />
+    </div>
   );
 };
 export default TestimonialCardPage;
--- components/testimonialCard/designs/TestimonialCard.tsx
+++ components/testimonialCard/designs/TestimonialCard.tsx
@@ -13,28 +13,26 @@ const TestimonialCard = ({
   message,
 }: TestimonialCardProps) => {
   return (
-    <div className="container-layout flex min-h-screen min-w-screen justify-center bg-linear-to-b from-gray-50 to-[#d2d6db]">
-      <div className="mt-50 flex h-full max-w-85 flex-col gap-4 rounded-lg bg-white p-6 shadow-sm">
-        <div className="flex items-center gap-4">
-          <Image
-            src={image}
-            alt={name}
-            width={48}
-            height={48}
-            className="object-cover"
-          />
+    <div className="mt-50 flex h-full max-w-85 flex-col gap-4 rounded-lg bg-white p-6 shadow-sm">
+      <div className="flex items-center gap-4">
+        <Image
+          src={image}
+          alt={name}
+          width={48}
+          height={48}
+          className="object-cover"
+        />
 
-          <div className="flex flex-col gap-px">
-            <h3 className="text-lg font-semibold text-neutral-900">{name}</h3>
-            <span className="text-sm font-normal text-neutral-600">
-              {username}
-            </span>
-          </div>
+        <div className="flex flex-col gap-px">
+          <h3 className="text-lg font-semibold text-neutral-900">{name}</h3>
+          <span className="text-sm font-normal text-neutral-600">
+            {username}
+          </span>
         </div>
+      </div>
 
-        <div className="text-base font-normal text-neutral-600">
-          <p>{message}</p>
-        </div>
+      <div className="text-base font-normal text-neutral-600">
+        <p>{message}</p>
       </div>
     </div>
   );
